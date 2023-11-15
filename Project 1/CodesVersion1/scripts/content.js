235be0e5f1fc4fd21f88da6059cc8f0a34e8cbc1
Ivyan <ds26733@gmail.com>
2023-11-15 23:35:56 +0530

    Task achieved
    Progress going on

--- Project 1/CodesVersion1/scripts/content.js
+++ Project 1/CodesVersion1/scripts/content.js
@@ -1,1 +1,18 @@
-// console.log("Hello World");
+let body = document.querySelector("body");
+
+
+let btnBQ = document.createElement("button");
+btnBQ.setAttribute("id" , "btnBQ");
+btnBQ.addEventListener("click", dosomething);
+body.appendChild(btnBQ);
+
+function dosomething(){
+    if(btnBQ.hasAttribute("listening")==false){
+        btnBQ.setAttribute("listening",true);
+        console.log("I am all ears")
+    }else{
+        btnBQ.removeAttribute("listening");
+        console.log("I can't hear you anymore");
+    }
+}
+
